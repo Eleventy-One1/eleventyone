9c0534c6b6f5e256925c785df6e8dd810ac7d9de
Sean Messner <seanmessner@Sean-Messners-MacBook-Pro.local>
2013-12-09 00:37:55 -0600

    fixed contact form

--- sinatra/sinatra_app.rb
+++ sinatra/sinatra_app.rb
@@ -4,13 +4,15 @@ set :public_dir, Proc.new { File.join(root, "_site") }
 set :views, Proc.new { File.join(File.dirname(__FILE__), "views") }
 
 post '/contact', :provides => :json do
-  msg = params.to_s
-  smtp = Net::SMTP.new 'smtp.gmail.com', 587
-  smtp.enable_starttls
-  smtp.start(ENV['EMAIL_DOMAIN'], ENV['GMAIL_USERNAME'], ENV['GMAIL_PASSWORD'], :login) do
-    smtp.send_message(msg, ENV["GMAIL_ADDRESS"], ENV["GMAIL_ADDRESS"])
+   msg = params.to_s
+   smtp = Net::SMTP.new 'smtp.gmail.com', 587
+   smtp.enable_starttls
+   smtp.start(ENV['EMAIL_DOMAIN'], ENV['GMAIL_USERNAME'], ENV['GMAIL_PASSWORD'], :login) do
+     smtp.send_message(msg, ENV["GMAIL_ADDRESS"], ENV["GMAIL_ADDRESS"])
   end
+redirect "/index.html"
 end
+
 
 before do
     response.headers['Cache-Control'] = 'public, max-age=36000'
